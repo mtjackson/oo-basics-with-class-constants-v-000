67a3013e0f1b1d99c86d09ad4166f5f8d14a530e
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-29 20:19:31 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,16 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.each do |brand|
-      i = 0
-      if BRANDS.length == 0
-        BRANDS << brand
-      end
-      while i < BRANDS.length
-      while i < BRANDS.length || BRANDS.length == 0
-        if BRANDS[i] != brand
-          BRANDS << brand
-        end
+    if BRANDS.include?(brand) == false
+      BRANDS << brand
+    end
   end
 
   def cobble
